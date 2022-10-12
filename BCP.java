df250a5d8ac5659b5ebc2f5f1f565e42af6175d1
Abreu <abreu@disroot.org>
2022-10-12 12:16:54 -0300

    Indo para o próximo arquivo

--- BCP.java
+++ BCP.java
@@ -1,37 +1,60 @@
 package escalonador;
 
+public class BCP implements Comparable<BCP> {
+    private int PC, estado, prioridade, creditos, espera, X, Y;
+    private boolean executado;
+    private String[] instrucoes;
+    private String nome;
 
-public class BCP implements Comparable<BCP>{
-	private int PC; //Program Counter
-	private int estado; //0 = bloqueado 1 = pronto
-	private int prioridade;
-	private int creditos;
-	private int X,Y;
-	private int flag;
-	private String [] instrucoes;
-	private String nome;
+    public BCP(String nome, String[] instrucoes, int prioridade) {
+        PC = 0;
+        X = 0;
+        Y = 0;
+        estado = 1;
+        this.nome = nome;
+        this.instrucoes = instrucoes;
+        this.prioridade = prioridade;
+        this.creditos = prioridade;
+    }
 
-	public BCP(String nome, String [] instrucoes, int prioridade) {
-		PC = 0;
-		X = 0;
-		Y = 0;
-		estado = 1;
-		this.nome = nome;
-		this.instrucoes = instrucoes;
-		this.prioridade = prioridade;
-		this.creditos = prioridade;
-	}
-
-	public consomeCredito() {
+    public consomeCredito() {
 		creditos--;
 	}
 
-	public int compareTo(BCP bcp) {
-		if(this.creditos > bcp.creditos) return 1;
-		if(this.creditos == bcp.creditos) return 0;
-		return -1;
-	}
-	public void print() {
-		for(int i = 0; i < instrucoes.length; i++) System.out.println(instrucoes[i]);
-	}
+    public int compareTo(BCP bcp) {
+        if (this.creditos > bcp.creditos)
+            return 1;
+        if (this.creditos == bcp.creditos)
+            return 0;
+        return -1;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public int getEspera() {
+        return espera;
+    }
+
+    public void setEspera(int espera) {
+        this.espera = espera;
+    }
+
+    public void print() {
+        for (int i = 0; i < instrucoes.length; i++)
+            System.out.println(instrucoes[i]);
+    }
 }
--- escalonador.java
+++ escalonador.java
@@ -8,156 +8,146 @@ import java.io.PrintStream;
 
 public class Escalonador {
 
-	private static int quantum;
-	private static int interrupcoes = 0;
-	private static int instrucoes = 0;
-	private static int contQuantuns = 0;
-	private final static int processos = 10;
+    private static int quantum;
+    private static int interrupcoes = 0;
+    private static int instrucoes = 0;
+    private static int contQuantuns = 0;
+    private final static int processos = 10;
 
-	public static void main(String[] args) throws Exception {
-		Leitura leitura = new Leitura();
-		int cont;
-		boolean termino
-		boolean suspenso
-		String instrucao
+    public static void main(String[] args) throws Exception {
+        Leitura leitura = new Leitura();
+        int quantum = leitura.getQuantum();
 
-		quantum = leitura.getQuantum();
+        criarLog(quantum);
 
-		// criando o arquivo LOG
-		if (quantum < 10) {
-			System.setOut(new PrintStream( new FileOutputStream("log"+quantum + ".txt")));
-		} else {
-			System.setOut(new PrintStream( new FileOutputStream("log"+quantum + ".txt")));
-		}
+        for (BCP bcp : prontos) {
+            System.out.println("Carregando processo " + bcp.processo.nome);
+        }
 
+        while (prontos.size() > 0 || bloqueados.size() > 0) {
+            administraProcesso(TabelaProcessos.removePrimeiroPronto(), quantum);
+            decrementaEsperaBloqueados();
+            verificaEspera();
+            verificaCreditosProntos();
+        }
 
-		for (BCP bcp : prontos) {
-			System.out.println("Carregando processo " + bcp.processo.nome);
-		}
+        System.out.println("media de trocas: " + interrupcoes / processos);
 
-		while (prontos.size() > 0 || bloqueados.size() > 0) {
-			if (prontos.size() > 0) { // Se tiver algum processo pronto
+        System.out.println("media de instruções: " + instrucoes / contQuantuns);
 
-				cont = 0;
-				termino = false;
-				suspenso = false;
+        System.out.println("quantum: " + quantum);
 
-				BCP atual = TabelaProcessos.tiraPrimeiroPronto();
-				atual.consomeCredito();
-				atual.processo.setEstado(Estado.EXECUTANDO);
-				System.out.println("Executando processo " + atual.processo.nome);
+    }
 
-				while (atual != null && cont < quantum) {
-					instrucao = atual.processo.instrucao[atual.PC];
+    private static void criaLog() {
+        if (quantum < 10) {
+            System.setOut(new PrintStream(new FileOutputStream("log0" + quantum + ".txt")));
+        } else {
+            System.setOut(new PrintStream(new FileOutputStream("log" + quantum + ".txt")));
+        }
+    }
 
-					if (instrucao.equals("E/S")) {
-						if (atual.flag == 0) {
-							System.out.println("E/S iniciada em " +atual.processo.nome);
-							entradaSaida(atual);
-							suspenso = true;
-							break;
-						} else {
-							atual.flag = 0;
-						}
-					}
+    private static void administraProcesso(BCP atual, int quantum) {
+        int cont = 0;
+        boolean termino = false, suspenso = false;
+        atual.consomeCredito();
+        atual.processo.setEstado(Estado.EXECUTANDO);
+        System.out.println("Executando processo " + atual.processo.nome);
+        while (atual != null && cont < quantum) {
+            administraInstrucao(atual, termino, saida);
+            atual.PC++;
+            cont++;
+        }
+        if (!termino) {
+            if (cont < 2) {
+                System.out.println("Interrompendo " + atual.processo.nome + " após " + cont + " instrução");
+            } else {
+                System.out.println("Interrompendo " + atual.processo.nome + " após " + cont + " instruções");
+            }
+        }
 
-					else if (instrucao.equals("SAIDA")) {
-						System.out.println(atual.processo.nome + " terminou. Sendo, X=" + atual.registradorX + " e Y=" + atual.registradorY);
-						prontos.remove(atual);
-						suspenso = true;
-						termino = true;
-						break;
-					}
+        interrupcoes++;
+        contQuantuns++;
+        instrucoes += cont;
 
-					else if (instrucao.contains("X=")) {
-						atual.registradorX = Integer.parseInt(instrucao.substring(2));
-					}
+        if (!suspenso) {
+            atual.processo.setEstado(Estado.PRONTO);
+            TabelaProcessos.adicionaBlocoProntos(atual);
+        }
+    }
 
-					else if (instrucao.contains("Y=")) {
-						atual.registradorY = Integer.parseInt(instrucao.substring(2));
-					}
+    private static void administraInstrucao(BCP atual) {
+        String instrucao = atual.processo.instrucao[atual.PC];
 
-					atual.PC++;
-					cont++;
-				}
+        if (instrucao.equals("E/S")) {
+            if (!atual.executado) {
+                System.out.println("E/S iniciada em " + atual.processo.nome);
+                entradaSaida(atual);
+                suspenso = true;
+                break;
+            } else {
+                atual.executado = false;
+            }
+        }
 
-				if (!termino) {
-					if (cont <= 1) {
-						System.out.println("Interrompendo processo "	+ atual.processo.nome + " após " + cont + " instrução");
-					} else {
-						System.out.println("Interrompendo processo "	+ atual.processo.nome + " após " + cont + " instruções");
-					}
-				}
+        else if (instrucao.equals("FIM")) {
+            System.out.println(atual.processo.nome + " terminou. Sendo, X=" + atual.registradorX + " e Y="
+                    + atual.registradorY);
+            prontos.remove(atual);
+            suspenso = true;
+            termino = true;
+            break;
+        }
 
-				interrupcoes++;
-				contQuantuns++;
-				instrucoes += cont;
+        else if (instrucao.contains("X=")) {
+            atual.registradorX = Integer.parseInt(instrucao.substring(2));
+        }
 
-				if (!parou) {
-					atual.processo.setEstado(Processo.PRONTO);
-					TabelaProcessos.adicionaBlocoProntos(atual);
-				}
-			}
+        else if (instrucao.contains("Y=")) {
+            atual.registradorY = Integer.parseInt(instrucao.substring(2));
+        }
+    }
 
-			decrementaBloqueados(); //decrementa a espera de cada processo da lista de bloqueados
-			verificaZeroEspera(); //verifica se tem algum processo na lista de bloqueados com 0 de espera
-			verificaZeroCreditosProntos(); //verifica se todos os créditos da lista de prontos é 0, se sim já seta com a prioridade
-		}
+    private static void entradaSaida(BCP atual) {
+        atual.processo.setEstado(Estado.BLOQUEADO);
+        TabelaProcessos.adicionaBlocoBloqueados(atual);
+        int i = bloqueados.indexOf(atual);
+        bloqueados.get(index).espera = 2;
+        bloqueados.get(index).executado = true;
+    }
 
-		System.out.println("media de trocas: " + mediaTrocas(interrompido));
-		System.out.println("media de instruções: " + mediaInstrucao(instrucaoQuantum, numQuantum));
+    private static boolean verificaCreditosProntos() {
+        for (BCP p : prontos) {
+            if (p.getCreditos() != 0)
+                return false;
+        }
+        for (BCP p : prontos) {
+            p.setCreditos(p.getPrioridade());
+        }
+        return true;
+    }
 
-		System.out.println("quantum: " + quantum);
-	}
+    private static void decrementaBloqueados() {
+        for (BCP b : bloqueados) {
+            if (b.getEspera() > 0)
+                b.setEspera(b.getEspera() - 1);
+        }
+    }
 
-	private static double mediaTrocas (double interrompido) {
-		return interrompido / totalProcessos;
-	}
+    private static void verificaEspera() {
+        int cont = 0;
 
-	private static double mediaInstrucao (double instrucaoQuantum, double numQuantum) {
-		return instrucaoQuantum / numQuantum;
-	}
+        for (BCP b : bloqueados) {
+            if (b.getEspera() != 0) {
+                break;
+            }
+            b.processo.setEstado(Estado.PRONTO);
+            TabelaProcessos.adicionaBlocoProntos(b);
+            cont++;
+        }
 
-	private static void entradaSaida(BCP processo) {
-		processo.processo.setEstado(Processo.BLOQUEADO);
-		TabelaProcessos.adicionaBlocoBloqueados(processo); //adiciona o processo à lista de bloqueados
-		int index = bloqueados.indexOf(processo);
-		bloqueados.get(index).espera = 2; //atualiza a espera do processo para 2
-		bloqueados.get(index).flag = 1; //atualiza o flag para 1, ou seja, a instrução já foi executada
-	}
-
-	private static boolean verificaZeroCreditosProntos() {
-		for (BCP p : prontos) {
-			if (p.credito != 0)	return false; //se algum crédito for diferente de 0, já retorna falso
-		}
-		//se todos os créditos forem iguais a 0, atualiza todos os créditos à sua prioridade correspondente
-		for (BCP p : prontos) {
-			p.credito = p.prioridade;
-		}
-		return true;
-	}
-
-	private static void decrementaBloqueados() {
-		for (BCP b : bloqueados) {
-			if (b.espera > 0) b.espera--; //diminui 1 da espera
-		}
-	}
-
-	private static void verificaZeroEspera() {
-		int cont = 0;
-
-		//conta quantos processos tem a espera igual a zero
-		for (BCP b : bloqueados) {
-			if (b.espera == 0) {
-				b.processo.setEstado(Processo.PRONTO);
-				TabelaProcessos.adicionaBlocoProntos(b);
-				cont++;
-			} else break;
-		}
-
-		//remove todos os bloqueados com espera igual a zero
-		for (int i = 0; i < cont; i++) {
-			TabelaProcessos.removePrimeiroBloqueados();
-		}
-	}
+        for (int i = 0; i < cont; i++) {
+            TabelaProcessos.removePrimeiroBloqueado();
+        }
+    }
 }
