22abcf25442d1a76106e85f54d0ed46419fb2668
LumenRuby <88425511+LumenRuby@users.noreply.github.com>
2022-10-23 18:18:56 -0300

    Update Leitura.java

--- Leitura.java
+++ Leitura.java
@@ -1,45 +1,73 @@
 package escalonador;
 
 import java.io.*;
+import java.util.*;
 
 public class Leitura {
-    private int prioridade;
-    private String nome;
-    private String[] instrucoes;
-    private BCP bcp;
+    private ArrayList<BCP> Programas;
+    private int quantum;
 
-    public Leitura(String numArquivo) {
+    public Leitura(int numDeArquivos) {
         try {
-            instrucoes = new String[21]; // inicializando o array que guarda o texto do programa
+            Programas = leProgramas(numDeArquivos);
+            quantum = leQuantum();
 
-            BufferedReader in = new BufferedReader(new FileReader(numArquivo + ".txt"));
-            String Line = in.readLine();
-            nome = Line;// nome do arquivo
-
-            // lê o programa
-            Line = in.readLine();
-            for (int i = 0; Line != null; i++) {
-                instrucoes[i] = Line;
-                Line = in.readLine();
-            }
-            prioridade = lePrioridade(numArquivo);
-            // System.out.println(prioridade + nome);
-            bcp = new BCP(nome, instrucoes, prioridade);
-            in.close();
         } catch (FileNotFoundException e) {
-            System.out.println("File nome not found");
-            System.out.println(numArquivo + ".txt");
+            System.out.println("File name not found");
+            // System.out.println(numArquivo + ".txt");
         } catch (IOException e) {
             System.out.println("Invalid File format");
         }
     }
 
-    private int lePrioridade(String numArquivo) throws IOException {
-        BufferedReader in;
-        in = new BufferedReader(new FileReader("prioridades.txt"));
+    private ArrayList<BCP> leProgramas(int numDeArquivos) throws IOException {
+        ArrayList<BCP> Programas = new ArrayList<BCP>();
+
+        for (int i = 1; i <= numDeArquivos; i++) {
+            String[] buffer = new String[21]; // inicializando o array que guarda temporariamente as instruções do
+                                              // programa
+            String nomeDoArquivo = i < 10 ? "0" + Integer.toString(i) + ".txt" : Integer.toString(i) + ".txt";
+
+            BufferedReader in = new BufferedReader(new FileReader(nomeDoArquivo));
+            ; // in que irá conter as instruções de cada programa
+            // Encontrando o nome do arquivo
+            String Line = in.readLine();
+            String nome = Line;
+
+            // Lê as instruções
+            Line = in.readLine();
+            int j;
+            for (j = 0; Line != null; j++) {
+                buffer[j] = Line;
+                Line = in.readLine();
+            }
+
+            String[] instrucoes = new String[j];
+            for (int m = 0; m < j; m++)
+                instrucoes[m] = buffer[m]; // para evitar que as instruções sejam um array com vários null no fim, isso
+                                           // é necessário
+            int prioridade = lePrioridade(i); // lê prioridade
+
+            BCP bcp = new BCP(nome, instrucoes, prioridade); // criando o bcp
+            Programas.add(bcp); // adicionando o bcp à lista
+            in.close();
+        }
+        return Programas;
+
+    }
+
+    private int leQuantum() throws NumberFormatException, IOException {
+        BufferedReader in = new BufferedReader(new FileReader("quantum.txt"));
+        int quantum = Integer.parseInt(in.readLine());
+        in.close();
+        return quantum;
+    }
+
+    private int lePrioridade(int numArquivo) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("prioridades.txt"));
         int prioridade = 0;
         String Line = new String();
-        for (int i = 0; i < Integer.parseInt(numArquivo); i++) {
+        for (int i = 0; i < numArquivo; i++) {
             Line = in.readLine();
             prioridade = Integer.parseInt(Line);
         }
@@ -47,7 +75,11 @@ public class Leitura {
         return prioridade;
     }
 
-    public BCP getBCP() {
-        return this.bcp;
+    public int getQuantum() {
+        return quantum;
+    }
+
+    public BCP getBCP(int numArquivo) {
+        return Programas.get(numArquivo);
     }
 }
